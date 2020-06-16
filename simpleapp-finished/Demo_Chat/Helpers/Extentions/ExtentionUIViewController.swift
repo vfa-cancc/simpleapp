699e29345316ef94c8cf22e43346e2fc8f092690
HungNV <vfa.hungnv@gmail.com>
2020-06-16 15:48:50 +0700

    #update code uitest.

--- simpleapp-finished/Demo_Chat/Helpers/Extentions/ExtentionUIViewController.swift
+++ simpleapp-finished/Demo_Chat/Helpers/Extentions/ExtentionUIViewController.swift
@@ -87,9 +87,15 @@ extension UIViewController {
         
         //Left
         if leftImg != nil && leftSelector != nil {
-            vc.navigationItem.leftBarButtonItem = UIBarButtonItem(image: leftImg, style: .plain, target: vc, action: leftSelector)
+            let leftBar = UIBarButtonItem(image: leftImg, style: .plain, target: vc, action: leftSelector)
+            leftBar.accessibilityLabel = "leftBar"
+            
+            vc.navigationItem.leftBarButtonItem = leftBar
         } else if leftText != nil && leftSelector != nil {
-            vc.navigationItem.leftBarButtonItem = UIBarButtonItem(title: leftText, style: .plain, target: vc, action: leftSelector)
+            let leftBar = UIBarButtonItem(title: leftText, style: .plain, target: vc, action: leftSelector)
+            leftBar.accessibilityLabel = "leftBar"
+            
+            vc.navigationItem.leftBarButtonItem = leftBar
             vc.navigationItem.leftBarButtonItem?.tintColor = UIColor.black
             vc.navigationItem.leftBarButtonItem?.setTitleTextAttributes(textAttributes, for: .normal)
         } else {
@@ -98,9 +104,15 @@ extension UIViewController {
         
         //Right
         if rightImg != nil && rightSelector != nil {
-            vc.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightImg, style: .plain, target: vc, action: rightSelector)
+            let rightBar = UIBarButtonItem(image: rightImg, style: .plain, target: vc, action: rightSelector)
+            rightBar.accessibilityLabel = "rightBar"
+            
+            vc.navigationItem.rightBarButtonItem = rightBar
         } else if rightText != nil && rightSelector != nil {
-            vc.navigationItem.rightBarButtonItem = UIBarButtonItem(title: rightText, style: .plain, target: vc, action: rightSelector)
+            let rightBar = UIBarButtonItem(title: rightText, style: .plain, target: vc, action: rightSelector)
+            rightBar.accessibilityLabel = "rightBar"
+            
+            vc.navigationItem.rightBarButtonItem = rightBar
             vc.navigationItem.rightBarButtonItem?.tintColor = UIColor.black
             vc.navigationItem.rightBarButtonItem?.setTitleTextAttributes(textAttributes, for: .normal)
         } else {
---
+++ simpleapp-finished/HuCaChat_DevUITests/HuCaChat_DevUITests.swift
@@ -0,0 +1,138 @@
+//
+//  HuCaChat_DevUITests.swift
+//  HuCaChat_DevUITests
+//
+//  Created by HungNV on 6/16/20.
+//  Copyright © 2020 HungNV. All rights reserved.
+//
+
+import XCTest
+
+class HuCaChat_DevUITests: XCTestCase {
+    let app = XCUIApplication()
+    static let kUserInfo = "UI-TestingKey_kUserInfo"
+    
+    // MARK: - Setup for UI Test
+    override func setUp() {
+        /// In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        app.launchArguments.append("UI-Testing")
+        app.launchEnvironment[HuCaChat_DevUITests.kUserInfo] = "NO"
+        app.launch()
+    }
+
+    override func tearDown() {
+        /// Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    // MARK: - Handle UI Test
+    
+    /// Handle test all app
+    func testAllApp() {
+        self.testSignInVC()
+        self.testLogoutVC()
+        self.testSignInVC()
+        self.testRightVC()
+        self.testHomeVC()
+    }
+    
+    /// Handle test Login view cotroller
+    private func testSignInVC() {
+        let tfUsername = app.textFields["tfUsername"]
+        XCTAssert(tfUsername.exists)
+        tfUsername.tap()
+        tfUsername.typeText("vfa.hungnv@gmail.com")
+        
+        let tfPassword = app.secureTextFields["tfPassword"]
+        XCTAssert(tfPassword.exists)
+        self.tapElementAndWaitForKeyboardToAppear(tfPassword)
+        tfPassword.typeText("123456")
+        
+        let btnLogin = app.buttons["btnLogin"]
+        XCTAssert(btnLogin.exists)
+        btnLogin.tap()
+    }
+    
+    /// Handle test Login view cotroller
+    private func testLogoutVC() {
+        /// Logout
+        let leftBar: XCUIElement = app.navigationBars.buttons.element(boundBy: 0)
+        self.waitForElementToAppear(leftBar) /// Wait for element load finished
+        leftBar.tap()
+        
+        let btnLogout = app.buttons["btnLogout"]
+        XCTAssert(btnLogout.exists)
+        btnLogout.tap()
+    }
+    
+    /// Handle test Right view cotroller, list all user in blacklist
+    private func testRightVC() {
+        /// Right bar
+        let rightBar: XCUIElement = app.navigationBars.buttons.element(boundBy: 1)
+        self.waitForElementToAppear(rightBar) /// Wait for element load finished
+        XCTAssert(rightBar.exists)
+        rightBar.tap()
+        
+        let tableView = app.tables.containing(.table, identifier: "tableView")
+        XCTAssertTrue(tableView.cells.count > 0)
+        let firstCell = tableView.cells.element(boundBy: 0)
+        firstCell.swipeRight()
+        app.alerts.firstMatch.buttons.element(boundBy: 0).tap() /// Alert confirm not create.
+        rightBar.tap()
+    }
+    
+    /// Handle test Home view cotroller
+    private func testHomeVC() {
+        /// Send message
+        app.tables.staticTexts["Hung IT"].tap()
+        let tvInputMessage = app.textViews["tvInputMessage"]
+        XCTAssert(tvInputMessage.exists)
+        tvInputMessage.tap()
+        tvInputMessage.typeText("VnIndex bứt phá 22 điểm, nhiều nhà đầu tư chạy hôm qua tiếc nuối ^^")
+        let btnSend = app.buttons["btnSend"]
+        XCTAssert(btnSend.exists)
+        btnSend.tap()
+        app.navigationBars["Hung IT"].buttons["leftBar"].tap()
+        
+        /// Move to Group view controller
+        
+    }
+    
+    
+    
+    // MARK: - Other method
+    
+    /// Wait for element/ui to appear
+    private func waitForElementToAppear(_ element: XCUIElement, file: String = #file, line: UInt = #line) {
+        let existsPredicate = NSPredicate(format: "exists == true")
+        expectation(for: existsPredicate, evaluatedWith: element, handler: nil)
+        
+        waitForExpectations(timeout: 5) { (error) -> Void in
+            if (error != nil) {
+                let message = "Failed to find \(element) after 5 seconds."
+                self.recordFailure(withDescription: message, inFile: file, atLine: Int(line), expected: true)
+            }
+        }
+    }
+    
+    /// Wait for keyboard to appear
+    private func tapElementAndWaitForKeyboardToAppear(_ element: XCUIElement) {
+        let keyboard = XCUIApplication().keyboards.element
+        while (true) {
+            element.tap()
+            if keyboard.exists {
+                break;
+            }
+            RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.5))
+        }
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
